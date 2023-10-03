b9d5d9412d0937c365e2fcaf974918aafc34513e
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2023-10-03 23:59:45 +0530

    adding vectorization using inbuild functions like map, filter, reduce etc

--- loops_vs_vectorization/built_in_functions.py
+++ loops_vs_vectorization/built_in_functions.py
@@ -1,10 +1,9 @@
 import timeit
 
-# Original list and vectorized operation
-original_list = [1, 2, 3, 4, 5]
 
 def vectorized_operation(original_list):
     return list(map(lambda x: x * 2, original_list))
+
 
 # Non-vectorized operation using a for loop
 def non_vectorized_operation(original_list):
@@ -13,12 +12,15 @@ def non_vectorized_operation(original_list):
         result.append(x * 2)
     return result
 
-# Measure execution time for vectorized operation
-vectorized_time = timeit.timeit(lambda: vectorized_operation(original_list), number=1000000)
 
-# Measure execution time for non-vectorized operation
-non_vectorized_time = timeit.timeit(lambda: non_vectorized_operation(original_list), number=1000000)
+if __name__ == '__main__':
+    original_list = [1, 2, 3, 4, 5]
+    # Measure execution time for vectorized operation
+    vectorized_time = timeit.timeit(lambda: vectorized_operation(original_list), number=1000000)
 
-# Print the results
-print(f"Vectorized Operation Time: {vectorized_time:.6f} seconds")
-print(f"Non-Vectorized Operation Time: {non_vectorized_time:.6f} seconds")
+    # Measure execution time for non-vectorized operation
+    non_vectorized_time = timeit.timeit(lambda: non_vectorized_operation(original_list), number=1000000)
+
+    # Print the results
+    print(f"Time taken by Vectorized Operation: {vectorized_time:.6f} seconds")
+    print(f"Time taken by Non-Vectorized Operation: {non_vectorized_time:.6f} seconds")
