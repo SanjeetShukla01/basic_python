1cbccddc09ef5f07d043fb95e611c669e401d5dc
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2023-10-04 00:37:37 +0530

    adding cprofile code

--- profiling/profiling.py
+++ profiling/profiling.py
@@ -1,19 +1,22 @@
 import cProfile
 import random
 
+
 # Define a complex calculation function
 def complex_calculation(x):
     # Simulate a complex calculation, e.g., squaring the number
     result = x * x
     return result
 
+
 # Generate a list of random numbers for demonstration
 data = [random.randint(1, 1000) for _ in range(10000)]
-print(data)
+
 
 # Using the map function for vectorization
 def using_map():
     return list(map(complex_calculation, data))
+
 
 # Using a plain for loop
 def using_plain_loop():
@@ -21,6 +24,7 @@ def using_plain_loop():
     for num in data:
         result.append(complex_calculation(num))
     return result
+
 
 if __name__ == "__main__":
     print("Using map function:")
